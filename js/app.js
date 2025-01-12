3a3f7e2b79f5f02108817c66482fcb158df89910
derek-cassady <derek@cassadyweb.com>
2025-01-12 16:39:49 -0500

    setup DOM element variables in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 "use strict";
+
+// DOM elements from index.html
+
+// inputs
+const guessInput = document.getElementById("guess-input"); // line 20
+
+// readout
+const guessMessage = document.getElementById("guess-message"); //line 25
+const currentGuess = document.getElementById("current-guess"); //line 27
+const computerGuess = document.getElementById("computer-guess"); //line 30
+const guessHistory = document.getElementById("guess-history"); //line 33
+
+//buttons
+const submitBtn = document.getElementById("submit-btn"); //line 38
+const restartBtn = document.getElementById("restart-btn"); //line 39
